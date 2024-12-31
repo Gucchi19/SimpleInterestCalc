d61036c277f66ca736b22cb354b680e06ec8d4d1
Gucchi19 <76045850+Gucchi19@users.noreply.github.com>
2024-12-31 13:12:11 +0000

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+# Simple Interest Calculator
+
+## Input Principal amount, Interest Rate and Term or period
+Principal = float(input("Value of the Capital amount in £: "))
+Interest Rate = float(input("Interest Rate in %: )) 
+Term = int(input("period in years: ))
+
+## Calculate Simple Interest
+Simple Interest = Principal * Interest Rate * Term
+
+## Display the result
+print(f"Simple Interest:  GBP")
